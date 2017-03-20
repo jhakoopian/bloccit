6ccf7ffcd28decac5cc8e9e2dae8200d3f2bf464
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-03-19 23:06:22 -0400

    as16: install pry gem

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,10 +4,6 @@ RSpec.describe Answer, type: :model do
   let(:question) { Question.new(title: "New Question Title", body: "New Question Body", resolved: false) }
   let(:answer) { Answer.new(question: question, body: "New Answer Body") }
 
-  # it "should respond to body" do
-  #   expect(question).to respond_to(:body)
-  # end
-
   describe "attributes" do
     it "has a body attribute" do
       expect(answer).to have_attributes(body: "New Answer Body")

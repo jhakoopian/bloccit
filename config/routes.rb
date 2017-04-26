6e01f20d5ef47fae7dfbcd30b9888213121cdc1f
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-04-26 14:46:47 -0400

    as24: add authentication confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create]
 
+  post 'users/confirm' => 'users#confirm'
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'

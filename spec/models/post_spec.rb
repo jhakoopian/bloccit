5833528557a525b2f4fa1970e82c8ac7f81b3262
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-05-26 12:57:28 -0400

    cp30: in progress

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe Post, type: :model do
 
   let(:topic) { Topic.create!(name: name, description: description) }
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:userb) { User.create!(name: "Bloccit User B", email: "userb@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: title, body: body, user: user) }
 
   it { is_expected.to have_many(:comments) }
+  it { is_expected.to have_many(:votes) }
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
 
@@ -25,6 +27,33 @@ RSpec.describe Post, type: :model do
   describe "attributes" do
     it "has a title, body, and user attribute" do
       expect(post).to have_attributes(title: title, body: body, user: user)
+    end
+  end
+
+  describe "voting" do
+    before do
+      post.votes.create!(value: 1, user: user)
+      post.votes.create!(value: -1, user: userb)
+      @up_votes = post.votes.where(value: 1).count
+      @down_votes = post.votes.where(value: -1).count
+    end
+
+    describe "#up_votes" do
+      it "counts the number of votes with value = 1" do
+        expect( post.up_votes ).to eq(@up_votes)
+      end
+    end
+
+    describe "#down_votes" do
+      it "counts the number of votes with value = -1" do
+        expect( post.down_votes ).to eq(@down_votes)
+      end
+    end
+
+    describe "#points" do
+      it "returns the sum of all down and up votes" do
+        expect( post.points ).to eq(@up_votes - @down_votes)
+      end
     end
   end
 end
